12ebd5d9a2c80c38ab0a1353d8330fbad5692802
vapeman <kiv@vivaldi.net>
2022-01-24 16:38:23 +0300

    Update fabric canvas component

--- src/app/components/fabric-canvas/fabric-canvas.component.ts
+++ src/app/components/fabric-canvas/fabric-canvas.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, AfterViewInit, ElementRef, ViewChild} from '@angular/core';
+import {FabricResizeService} from "../../services/fabric-services/resize-service/fabric-resize.service";
+import {fabric} from "fabric";
 
 @Component({
   selector: 'app-fabric-canvas',
   templateUrl: './fabric-canvas.component.html',
   styleUrls: ['./fabric-canvas.component.less']
 })
-export class FabricCanvasComponent implements OnInit {
+export class FabricCanvasComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  _canvas: fabric.Canvas | undefined = undefined;
 
-  ngOnInit(): void {
+  constructor(private _fabricResizeService: FabricResizeService) { }
+
+  @ViewChild('canvas') private _canvasElement: ElementRef | undefined;
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    this._initFabric();
+  }
+
+  _initFabric() {
+    console.log(this._canvasElement?.nativeElement);
+    if(this._canvasElement !== undefined) {
+      this._canvas = new fabric.Canvas(this._canvasElement.nativeElement, {
+        selection: true,
+        controlsAboveOverlay: true,
+        centeredScaling: true,
+        allowTouchScrolling: true,
+        preserveObjectStacking: false,
+        backgroundColor: 'red',
+      });
+      this._fabricResizeService.init(this._canvas);
+    }
   }
 
 }
