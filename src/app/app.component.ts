a5054e3de6b49ffb523653dc33aa3bf38cfb8196
vapeman <kiv@vivaldi.net>
2022-02-25 17:52:58 +0300

    Update app component to use the work-area service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {fabric} from "fabric";
+import {WorkAreaService} from "./services/work-area-service/work-area.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rplidar-front';
+
+  constructor(private workAreaService: WorkAreaService) {
+  }
+
+  public canvasMounted: boolean = false;
+
+  public onCanvasMounted(canvas: fabric.Canvas) {
+    console.log('It works!');
+    this.canvasMounted = true;
+    this.workAreaService.init(canvas);
+  }
+
+  public onStartWorkAreaCreation() {
+    this.workAreaService.startCreation();
+  }
+  public onDiscardWorkAreaCreation() {
+    this.workAreaService.discardCreation();
+  }
+  public onApplyCreatedWorkArea() {
+    this.workAreaService.applyCreatedWorkArea();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FabricCanvasComponent } from './components/fabric-canvas/fabric-canvas.component';
 import { HttpClientModule } from "@angular/common/http";
+import { WorkAreaControllerViewComponent } from './components/work-area-controller-view/work-area-controller-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FabricCanvasComponent
+    FabricCanvasComponent,
+    WorkAreaControllerViewComponent
   ],
   imports: [
     BrowserModule,
