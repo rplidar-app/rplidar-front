50138a23ac0aca18b0cf589a810cac5196ddd7a0
vapeman <kiv@vivaldi.net>
2022-03-04 17:52:04 +0300

    Update scans-drawing.service.ts according to changes of the interface

--- src/app/services/scans-drawing-service/scans-drawing.service.ts
+++ src/app/services/scans-drawing-service/scans-drawing.service.ts
@@ -32,13 +32,13 @@ export class ScansDrawingService {
     interval(100).subscribe({
       next: () => {
         this._lidarService.scans.subscribe(groupOfScans => {
-          let scans = [...groupOfScans[0], ...groupOfScans[1]];
+          let points = [...groupOfScans.inside, ...groupOfScans.outside];
           if(this._canvas === null || this._centralDot === null) {
             return ;
           }
-          if(scans.length > this._dots.length) {
+          if(points.length > this._dots.length) {
             // console.log('before: ', this._dots.length, scans.length);
-            while (this._dots.length < scans.length) {
+            while (this._dots.length < points.length) {
               let dot = new fabric.Circle(this._dotsProps);
               // this._group.add(dot);
               this._canvas.add(dot);
@@ -47,22 +47,22 @@ export class ScansDrawingService {
             }
             // console.log('after: ', this._dots.length, scans.length);
           } else {
-            if(scans.length < this._dots.length) {
-              for(let i = scans.length - 1; i < this._dots.length; i += 1) {
+            if(points.length < this._dots.length) {
+              for(let i = points.length - 1; i < this._dots.length; i += 1) {
                 this._dots[i].opacity = 0;
                 this._dots[i].dirty = true;
               }
             }
           }
-          for(let i = 0; i < scans.length; i += 1) {
-            const coordinates = ScansDrawingService._getDotCoordinates(scans[i]);
+          for(let i = 0; i < points.length; i += 1) {
+            const coordinates = ScansDrawingService._getDotCoordinates(points[i]);
             // if(this._dots[i] === undefined) {
             //   console.log(this._dots.length, scans.length);
             //   return ;
             // }
             this._dots[i].left = coordinates.x;
             this._dots[i].top = coordinates.y;
-            this._dots[i].opacity = i < groupOfScans[0].length ? 1 : .25;
+            this._dots[i].opacity = i < groupOfScans.inside.length ? 1 : .25;
           }
           this._canvas.requestRenderAll();
         })
@@ -70,7 +70,7 @@ export class ScansDrawingService {
     });
   }
 
-  private static _getDotCoordinates(scan: LidarScanInterface) {
+  private static _getDotCoordinates(scan: number[]) {
     return {x: scan[0], y: scan[1]};
   }
 }
