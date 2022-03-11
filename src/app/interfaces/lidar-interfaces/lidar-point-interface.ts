c7b644f297d1a8687550d477bfa0afa6f27109ce
vapeman <kiv@vivaldi.net>
2022-03-11 11:16:55 +0300

    Work commit

--- src/app/interfaces/lidar-interfaces/lidar-point-interface.ts
+++ src/app/interfaces/lidar-interfaces/lidar-point-interface.ts
@@ -1,7 +1,7 @@
 export interface LidarPointInterface extends Array<number> {
   0: number; // an X coordinate of the point
-  1: number; // an Y coordinate of the point
-  2: number; // a quality of the point
+  1: number; // a Y coordinate of the point
+  2: number; // a quality level of the point
   3: number; // a distance to the point from lidar's head
   4: number; // an angle of the point
   length: 5;
